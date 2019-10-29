8c500159fb35a0f709755aebdfee2766895e7f75
tochsner <tobia.ochsner@hotmail.com>
2019-10-29 10:55:14 +0100

    Added classes for landmarks.

---
+++ Environment/Landmarks.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+class LandmarkTypes(Enum):
+    LANDMARK = 0
+    FOOD = 1
+    NEST = 2
+
+class Landmark():
+    """
+	* Describes a landmark with its x and y coordinates.
+    * Each Landmark has a unique ID.
+	"""	
+
+    last_id = 0
+
+    def __init__(self, x, y):
+        self.id = Landmark.last_id
+        Landmark.last_id += 1
+
+        self.x = x
+        self.y = y
+
+    def get_id(self):
+        return self.id
+
+    def get_coordinates(self):
+        return self.x, self.y
+
+    def get_type(self):
+        return LandmarkTypes.LANDMARK
+
+class FOOD(Landmark):
+    def __init__(self, x, y):
+        Landmark.__init__(self, x, y)
+
+    def get_type(self):
+        return LandmarkTypes.FOOD
+
+class NEST(Landmark):
+    def __init__(self, x, y):
+        Landmark.__init__(self, x, y)
+
+    def get_type(self):
+        return LandmarkTypes.NEST
